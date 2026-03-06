c0642a555636a6f7db8f0e368332e59a8fa05e6b
Anay <iamanaykrishna@gmail.com>
2026-03-05 20:14:19 -0500

    Fix App.tsx Router to use Vite BASE_URL as basename

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { ApplyPage } from "./pages/ApplyPage";
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <Layout>
         <Navbar />
